18c21e4f2d52e4dcf6bae7fe78dfac29df298ae6
chaoticsparks <ilya.andreev.it@gmail.com>
2017-09-25 21:59:43 +0300

    Implementing done and undone functions for tasks

--- app/todolist/todolist.component.js
+++ app/todolist/todolist.component.js
@@ -6,16 +6,24 @@ angular.
 			this.todolist = [];
 			this.donelist = [];
 
-			
-			this.doneCount = this.donelist.length;
 
-			this.newTaskAdd = function () {
+			this.newTaskAdd = function() {
 				this.todolist.push({
 					title: this.newTaskTitle,
 					done: false
 				});
 				this.todoCount = this.todolist.length;
 				this.newTaskTitle = '';
+			}
+
+			this.taskDone = function(taskIndex) {
+				this.donelist.push(this.todolist[taskIndex]);
+				this.todolist.splice(taskIndex, 1);
+			}
+
+			this.taskUndone = function(taskIndex) {
+				this.todolist.push(this.donelist[taskIndex]);
+				this.donelist.splice(taskIndex, 1);
 			}
 		}
 	});
