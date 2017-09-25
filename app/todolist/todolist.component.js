e83508874d441eee4bfba50baaf0e57028123ff7
chaoticsparks <ilya.andreev.it@gmail.com>
2017-09-25 12:59:24 +0300

    Adding todo and done lists and them controller and templates

--- app/todolist/todolist.component.js
+++ app/todolist/todolist.component.js
@@ -3,6 +3,10 @@ angular.
 	component('todolist', {
 		templateUrl: 'todolist/todolist.template.html',
 		controller: function AppListController() {
+			this.todolist = [];
+			this.donelist = [];
 
+			this.todoCount = this.todolist.length;
+			this.doneCount = this.donelist.length;
 		}
 	});
